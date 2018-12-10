5363b43ee38f83a63072a8338a9638687e0a6879
jithu-geon <jithu.antony@gadgeon.com>
2018-12-10 14:13:33 +0530

    initial

---
+++ MyWebApi/UI/MyAngularProject/libs/container/src/lib/container.module.spec.ts
@@ -0,0 +1,17 @@
+
+    import { async, TestBed } from '@angular/core/testing';
+    import { ContainerModule } from './container.module';
+    
+    describe('ContainerModule', () => {
+      beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          imports: [ ContainerModule ]
+        })
+        .compileComponents();
+      }));
+    
+      it('should create', () => {
+        expect(ContainerModule).toBeDefined();
+      });
+    });
+          
